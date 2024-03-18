1806903d0f17cfb760f23184b160b5ccafc66458
UNIM-AJL <161278737+UNIM-AJL@users.noreply.github.com>
2024-03-18 22:32:34 +1100

    Add files via upload

---
+++ Workbook/A2.js
@@ -0,0 +1,29 @@
+var data = [
+    {
+      errorType:
+        "<br><br><pre>Error: operation could not open 'Assignment Two'; operation not permitted // Please return on the 16.04.2024</pre><br>",
+    },
+  ];
+  
+  var allElements = document.getElementsByClassName("error-txt");
+  for (var j = 0; j < allElements.length; j++) {
+    var currentElementId = allElements[j].id;
+    var currentElementIdContent = data[0][currentElementId];
+    var element = document.getElementById(currentElementId);
+    var devTypeText = currentElementIdContent;
+  
+    // type code
+    var i = 0,
+      isTag,
+      text;
+    (function type() {
+      text = devTypeText.slice(0, ++i);
+      if (text === devTypeText) return;
+      element.innerHTML = text + `<span class='blinker'>&#32;</span>`;
+      var char = text.slice(-1);
+      if (char === "<") isTag = true;
+      if (char === ">") isTag = false;
+      if (isTag) return type();
+      setTimeout(type, 50);
+    })();
+  }
--- Workbook/main.js
+++ Workbook/main.js
@@ -1,7 +1,14 @@
 //Draggable Elements on subject.html
 dragElement(document.getElementById("mov-a"));
 dragElement(document.getElementById("mov-b"));
-
+dragElement(document.getElementById("mov-c"));
+dragElement(document.getElementById("mov-d"));
+dragElement(document.getElementById("mov-e"));
+dragElement(document.getElementById("mov-f"));
+dragElement(document.getElementById("mov-h"));
+dragElement(document.getElementById("mov-i"));
+dragElement(document.getElementById("mov-j"));
+dragElement(document.getElementById("mov-k"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -43,3 +50,37 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+
+var data = [
+    {
+      welcomeType:
+        "<span>Media queries allow you to apply CSS styles depending on a device's media type (such as print vs. screen) or other features or characteristics such as screen resolution or orientation, aspect ratio, browser viewport width or height (mobile vs. desktop), user preferences such as preferring reduced motion, data usage, or transparency. https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_media_queries/Using_media_queries</span><br/>",
+    },
+  ];
+  
+  var allElements = document.getElementsByClassName("welcome-txt");
+  for (var j = 0; j < allElements.length; j++) {
+    var currentElementId = allElements[j].id;
+    var currentElementIdContent = data[0][currentElementId];
+    var element = document.getElementById(currentElementId);
+    var devTypeText = currentElementIdContent;
+  
+    // type code
+    var i = 0,
+      isTag,
+      text;
+    (function type() {
+      text = devTypeText.slice(0, ++i);
+      if (text === devTypeText) return;
+      element.innerHTML = text + `<span class='blinker'>&#32;</span>`;
+      var char = text.slice(-1);
+      if (char === "<") isTag = true;
+      if (char === ">") isTag = false;
+      if (isTag) return type();
+      setTimeout(type, 50);
+    })();
+  }
+
+
+  document.getElementById("iframe").contentWindow.location.href;
